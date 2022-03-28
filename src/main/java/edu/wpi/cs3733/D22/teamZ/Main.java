ca5393490d333d5d9102e92d0f927d59582d8d59
ash-cmd <andrew.hariyanto@gmail.com>
2022-03-27 20:11:43 -0400

    Initialized DB (DO NOT TOUCH OR MODIFY THANK U)

--- src/main/java/edu/wpi/cs3733/D22/teamZ/Main.java
+++ src/main/java/edu/wpi/cs3733/D22/teamZ/Main.java
@@ -1,8 +1,12 @@
 package edu.wpi.cs3733.D22.teamZ;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
 
     public static void main(String[] args) {
-        App.launch(App.class, args);
+        // App.launch(App.class, args);
     }
 }
