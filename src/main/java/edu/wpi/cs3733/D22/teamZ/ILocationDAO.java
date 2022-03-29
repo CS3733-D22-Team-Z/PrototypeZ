467a980f65934c5a961745ae4e8e728b40602901
ash-cmd <andrew.hariyanto@gmail.com>
2022-03-29 00:59:40 -0400

    Created MedEquipReq table

--- src/main/java/edu/wpi/cs3733/D22/teamZ/ILocationDAO.java
+++ src/main/java/edu/wpi/cs3733/D22/teamZ/ILocationDAO.java
@@ -3,13 +3,13 @@ package edu.wpi.cs3733.D22.teamZ;
 import java.util.List;
 
 public interface ILocationDAO {
-  List<Location> getAllLocations();
+    List<Location> getAllLocations();
 
-  Location getLocationByID(String nodeID);
+    Location getLocationByID(String nodeID);
 
-  void addLocation(Location loc);
+    void addLocation(Location loc);
 
-  void updateLocation(Location loc);
+    void updateLocation(Location loc);
 
-  void deleteLocation(Location loc);
+    void deleteLocation(Location loc);
 }
