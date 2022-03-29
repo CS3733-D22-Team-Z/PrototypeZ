bf61f3a9572e6d01dfe9253dc5462c410c25623e
ash-cmd <andrew.hariyanto@gmail.com>
2022-03-28 20:08:00 -0400

    Added function implementation for get all locations and get location by ID

--- src/main/java/edu/wpi/cs3733/D22/teamZ/LocationDAOImpl.java
+++ src/main/java/edu/wpi/cs3733/D22/teamZ/LocationDAOImpl.java
@@ -9,14 +9,65 @@ public class LocationDAOImpl implements ILocationDAO{
     List <Location> locations;
 
     public LocationDAOImpl(){
-        locations = new ArrayList<Location>();
+        locations = new ArrayList<>();
     }
 
     public List<Location> getAllLocations(){
+        Connection connection = null;
+        try{
+            connection = DriverManager.getConnection("jdbc:derby:myDB");
+            PreparedStatement pstmt = connection.prepareStatement("Select * From Location");
+            ResultSet rset = pstmt.executeQuery();
+
+            while (rset.next()){
+                String nodeID = rset.getString("nodeID");
+                int xcoord = rset.getInt("xcoord");
+                int ycoord = rset.getInt("ycoord");
+                String floor = rset.getString("floor");
+                String building = rset.getString("building");
+                String nodeType = rset.getString("nodeType");
+                String longName = rset.getString("longName");
+                String shortName = rset.getString("shortName");
+                Location loc = new Location(nodeID, xcoord, ycoord, floor, building,nodeType,longName,shortName);
+                if(!locations.contains(loc)){
+                    locations.add(loc);
+                }
+            }
+        }
+        catch (SQLException e) {
+            System.out.println("Failed to get all Locations");
+        }
         return locations;
     }
     public Location getLocation(String nodeID){ //implement
-        return new Location();
+        Connection connection = null;
+        Location loc = new Location();
+        try{
+            connection = DriverManager.getConnection("jdbc:derby:myDB");
+            PreparedStatement pstmt = connection.prepareStatement("Select * From LOCATION WHERE NODEID =" + nodeID);
+            ResultSet rset = pstmt.executeQuery();
+
+            while (rset.next()){
+                int xcoord = rset.getInt("xcoord");
+                int ycoord = rset.getInt("ycoord");
+                String floor = rset.getString("floor");
+                String building = rset.getString("building");
+                String nodeType = rset.getString("nodeType");
+                String longName = rset.getString("longName");
+                String shortName = rset.getString("shortName");
+                loc.setNodeID(nodeID);
+                loc.setXcoord(xcoord);
+                loc.setYcoord(ycoord);
+                loc.setFloor(floor);
+                loc.setBuilding(building);
+                loc.setNodeType(nodeType);
+                loc.setLongName(longName);
+                loc.setShortName(shortName);
+            }
+        }catch (SQLException e){
+            System.out.println("Unable to find location");
+        }
+        return loc;
     }
     public void addLocation(Location loc){
         locations.add(loc);
