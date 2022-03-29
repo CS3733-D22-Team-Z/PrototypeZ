c07b07972e79a5948bcc18c328c18e6836facf2a
nadiaz2 <nadiaz2@wpi.edu>
2022-03-28 20:25:03 -0400

    Update App.java indentation

--- src/main/java/edu/wpi/cs3733/D22/teamZ/App.java
+++ src/main/java/edu/wpi/cs3733/D22/teamZ/App.java
@@ -15,28 +15,28 @@ import java.io.IOException;
 @Slf4j
 public class App extends Application {
 
-  @Override
-  public void init() {
-    log.info("Starting Up");
-  }
+    @Override
+    public void init() {
+        log.info("Starting Up");
+    }
 
-  @Override
-  public void start(Stage primaryStage) throws IOException {
-    Text text = new Text(1.0, 2.0, "Custom Font");
-    Font font = Font.loadFont("resources/fonts/Montserrat-Bold.ttf", 45);
-    text.setFont(font);
-    text.setFill(Color.BROWN);
-    text.setStroke(Color.BLUEVIOLET);
-    text.setStrokeWidth(0.5);
-    Parent root = FXMLLoader.load(App.class.getResource("views/app.fxml"));
-    Scene scene = new Scene(root);
-    primaryStage.setTitle("Custom Font");
-    primaryStage.setScene(scene);
-    primaryStage.show();
-  }
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Text text = new Text(1.0, 2.0, "Custom Font");
+        Font font = Font.loadFont("resources/fonts/Montserrat-Bold.ttf", 45);
+        text.setFont(font);
+        text.setFill(Color.BROWN);
+        text.setStroke(Color.BLUEVIOLET);
+        text.setStrokeWidth(0.5);
+        Parent root = FXMLLoader.load(App.class.getResource("views/app.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Custom Font");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
-  @Override
-  public void stop() {
-    log.info("Shutting Down");
-  }
+    @Override
+    public void stop() {
+        log.info("Shutting Down");
+    }
 }
