af38423f438bf87ff4f3e45400fdd53a5bf29f21
oliviaraisbeck <oeraisbeck@wpi.edu>
2022-03-28 20:41:16 -0400

    update delete add

--- src/main/java/edu/wpi/cs3733/D22/teamZ/LocationDAOImpl.java
+++ src/main/java/edu/wpi/cs3733/D22/teamZ/LocationDAOImpl.java
@@ -1,6 +1,5 @@
 package edu.wpi.cs3733.D22.teamZ;
 
-import javax.xml.transform.Result;
 import java.sql.*;
 import java.util.*;
 
@@ -73,59 +72,60 @@ public class LocationDAOImpl implements ILocationDAO {
     return loc;
   }
 
-    public void addLocation(Location loc) {
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection("jdbc:derby:myDB");
-            PreparedStatement stmt =
-                    connection.prepareStatement("INSERT INTO Location (NODEID, XCOORD, YCOORD, FLOOR, BUILDING, NODETYPE, LONGNAME, SHORTNAME)");
-            stmt.setString(1, loc.getNodeID());
-            stmt.setInt(2, loc.getXcoord());
-            stmt.setInt(3, loc.getYcoord());
-            stmt.setString(4, loc.getFloor());
-            stmt.setString(5, loc.getBuilding());
-            stmt.setString(6, loc.getNodeType());
-            stmt.setString(7, loc.getLongName());
-            stmt.setString(8, loc.getShortName());
+  public void addLocation(Location loc) {
+    Connection connection = null;
+    try {
+      connection = DriverManager.getConnection("jdbc:derby:myDB");
+      PreparedStatement stmt =
+          connection.prepareStatement(
+              "INSERT INTO Location (NODEID, XCOORD, YCOORD, FLOOR, BUILDING, NODETYPE, LONGNAME, SHORTNAME)");
+      stmt.setString(1, loc.getNodeID());
+      stmt.setInt(2, loc.getXcoord());
+      stmt.setInt(3, loc.getYcoord());
+      stmt.setString(4, loc.getFloor());
+      stmt.setString(5, loc.getBuilding());
+      stmt.setString(6, loc.getNodeType());
+      stmt.setString(7, loc.getLongName());
+      stmt.setString(8, loc.getShortName());
 
-            stmt.executeUpdate();
-            connection.commit();
-        } catch (SQLException e) {
-            System.out.println("Statement failed");
-        }
-        locations.add(loc);
+      stmt.executeUpdate();
+      connection.commit();
+    } catch (SQLException e) {
+      System.out.println("Statement failed");
     }
+    locations.add(loc);
+  }
 
-    public void updateLocation(Location loc) {
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection("jdbc:derby:myDB");
-            PreparedStatement stmt =
-                    connection.prepareStatement("UPDATE Location SET floor=?, nodeTYPE =? WHERE nodeID =?");
-            stmt.setString(1, loc.getFloor());
-            stmt.setString(2, loc.getNodeType());
-            stmt.setString(3, loc.getNodeID());
+  public void updateLocation(Location loc) {
+    Connection connection = null;
+    try {
+      connection = DriverManager.getConnection("jdbc:derby:myDB");
+      PreparedStatement stmt =
+          connection.prepareStatement("UPDATE Location SET floor=?, nodeTYPE =? WHERE nodeID =?");
+      stmt.setString(1, loc.getFloor());
+      stmt.setString(2, loc.getNodeType());
+      stmt.setString(3, loc.getNodeID());
 
-            stmt.executeUpdate();
-            connection.commit();
-        } catch (SQLException e) {
-            System.out.println("Statement failed");
-        }
-        locations.remove(loc);
-        locations.add(loc);
+      stmt.executeUpdate();
+      connection.commit();
+    } catch (SQLException e) {
+      System.out.println("Statement failed");
     }
+    locations.remove(loc);
+    locations.add(loc);
+  }
 
-    public void deleteLocation(Location loc) {
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection("jdbc:derby:myDB");
-            PreparedStatement stmt3 = connection.prepareStatement("DELETE FROM Location WHERE Nodeid=?");
-            stmt3.setString(1, loc.getNodeID());
-            stmt3.execute();
-            connection.commit();
-        } catch (SQLException e) {
-            System.out.println("Statement failed");
-        }
-        locations.remove(loc);
+  public void deleteLocation(Location loc) {
+    Connection connection = null;
+    try {
+      connection = DriverManager.getConnection("jdbc:derby:myDB");
+      PreparedStatement stmt3 = connection.prepareStatement("DELETE FROM Location WHERE Nodeid=?");
+      stmt3.setString(1, loc.getNodeID());
+      stmt3.execute();
+      connection.commit();
+    } catch (SQLException e) {
+      System.out.println("Statement failed");
     }
+    locations.remove(loc);
+  }
 }
--- src/main/java/edu/wpi/cs3733/D22/teamZ/Main.java
+++ src/main/java/edu/wpi/cs3733/D22/teamZ/Main.java
@@ -1,9 +1,6 @@
 package edu.wpi.cs3733.D22.teamZ;
 
-import java.util.List;
-
 public class Main {
 
-  public static void main(String[] args) {
-  }
+  public static void main(String[] args) {}
 }
